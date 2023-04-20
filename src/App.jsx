6772e588746a19518fdf9318206759a649612ba3
Giedrius_Simonavicius <giedrius.simonavicius123@gmail.com>
2023-04-20 11:01:45 +0300

    empty header / routes works

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/layout/Header";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ShopsPage from "./pages/ShopsPage";
+import AddShopPage from "./pages/AddShopPage";
+
 function App() {
-  return <div>New project</div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/shops" element={<ShopsPage />} />
+        <Route path="/shops/add" element={<AddShopPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
