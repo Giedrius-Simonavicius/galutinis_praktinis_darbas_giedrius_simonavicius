42e1877f97c61907689d448f3d53d6dcd85c2037
Giedrius_Simonavicius <giedrius.simonavicius123@gmail.com>
2023-04-20 12:07:04 +0300

    header structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ShopsPage from "./pages/ShopsPage";
 import AddShopPage from "./pages/AddShopPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div>
       <Header />
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/shops" element={<ShopsPage />} />
-        <Route path="/shops/add" element={<AddShopPage />} />
+        <Route path="/shops/new" element={<AddShopPage />} />
       </Routes>
     </div>
   );
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,20 @@
 import { NavLink, Link } from "react-router-dom";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div>
+        <Link to={"/"}>/ S H O P S \_ /\ _/ L O G O \</Link>
+        <nav>
+          <NavLink to={"/"}>Home page</NavLink>
+          <NavLink to={"/login"}>Login</NavLink>
+          <NavLink to={"/register"}>Register</NavLink>
+          <NavLink to={"/shops"}>Shops</NavLink>
+          <NavLink to={"/shops/new"}>Add shop</NavLink>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
