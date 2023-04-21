87875254aeffce2e1de93af41e302b5510f68c2a
Giedrius_Simonavicius <giedrius.simonavicius123@gmail.com>
2023-04-21 14:04:20 +0300

    404 auto redirect to login

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,21 @@
-import { NavLink } from 'react-router-dom';
 import '../styles/formPage.scss';
+import { useAuthCtx } from '../store/AuthProvider';
+import { useState } from 'react';
 function NotFound() {
+  const { navTo } = useAuthCtx();
+  const [timer, setTimer] = useState(5);
+
+  setTimeout(() => {
+    setTimer(timer - 1);
+  }, 1000);
+  setTimeout(() => {
+    navTo('login');
+  }, 5000);
+
   return (
     <div className="mainForm tac">
       <h1>404 - Page not found </h1>
-      <NavLink className="clickToReg" to="login">
-        Go to login page
-      </NavLink>
+      <h2>Redirecting to login page in: {timer}</h2>
     </div>
   );
 }
