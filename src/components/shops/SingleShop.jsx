826dea357a0ccf7f578661691229be2e59611992
Giedrius_Simonavicius <giedrius.simonavicius123@gmail.com>
2023-04-22 15:36:05 +0300

    shops page style

--- src/components/shops/SingleShop.jsx
+++ src/components/shops/SingleShop.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import './singleShop.scss';
 
 function SingleShop({ item }) {
   return (
     <li>
-      <img src={item.ImageUrl} alt={item.shopName} />
-      <h3>{item.shopName}</h3>
-      <p>{item.description}</p>
-      <p>
-        {item.town}, open since {item.startYear}
-      </p>
+      <div className="imgCont">
+        <img src={item.ImageUrl} alt={item.shopName} />
+      </div>
+      <h3 className="shopName">{item.shopName}</h3>
+      <div className="shopInfo">
+        <p className="description">{item.description}</p>
+        <p className="town">
+          Located in <span className="highlight">{item.town}</span>
+        </p>
+        <p>
+          Open since <span className="highlight">{item.startYear}</span>
+        </p>
+      </div>
     </li>
   );
 }
